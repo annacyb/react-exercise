2a382919fb955b451091b7fbb77686bec096927b
AnnaCybulska <anna357y@stud.kea.dk>
2021-11-16 12:28:02 +0100

    Added event on click to console product id

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,9 @@
 export default function Product(props) {
     // console.log(props);
-    
+    function handleClick() {
+        console.log(props.id);
+    }
+
     return (
         <article className="Product" style={{
             border: props.soldOut ? "green 2px solid" : "red  2px solid"
@@ -8,7 +11,9 @@ export default function Product(props) {
             <h2>{props.productdisplayname}</h2>
             <p>ID: {props.id}</p>
             <p>Price: {props.price}</p>
+            <button onClick={handleClick}>Click me</button>
         </article>
+        
     );
 }
 
